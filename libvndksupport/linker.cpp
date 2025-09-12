eb7ccbfbf78d6021c936207d44e2c9dc508327fe
烂泥扶上墙 <2114486405@qq.com>
2025-09-12 20:22:50 +0800

    Update linker.cpp

--- libvndksupport/linker.cpp
+++ libvndksupport/linker.cpp
@@ -1,6 +1,6 @@
 #include "platform/system/core/libvndksupport/linker.cpp"
 
-[[gnu::visibility("hidden"), gnu::always_inline]]
+[[gnu::visibility("hidden"), gnu::always_inline, gnu::weak]]
 android_namespace_t* android_get_exported_namespace(const char*) {
   return nullptr;
 }
